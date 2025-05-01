0b56547e0cebd16ba2f6b0212e0145802c96a28c
Dinar Sabangulov <dinarsabangulov@192.168.1.100>
2025-05-01 21:53:13 +0300

    First task

---
+++ date.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const {hideBin} = require('yargs/helpers')
+
+const now = new Date();
+
+const argv = yargs(hideBin(process.argv))
+    .command('current', 'Текущая дата и время', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'boolean',
+                description: 'Текущий год'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'boolean',
+                description: 'Текущий месяц'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'boolean',
+                description: 'Текущий день'
+            })
+    }, (argv) => {
+        if (argv.year) {
+            console.log(`<Текущий год: ${now.getFullYear()}`)
+        }
+        if (argv.month) {
+            console.log(`<Текущий месяц: ${now.getMonth() + 1}`)
+        }
+        if (argv.day) {
+            console.log(`<Текущий день: ${now.getDate()}`)
+        }
+    })
+    .argv
+
