c5b9be36d0b9525054d2c7d624bd95d18262bbbd
Dinar <79248748+dinero-s@users.noreply.github.com>
2025-05-02 14:50:44 +0300

    Update date.js

--- date.js
+++ date.js
@@ -34,7 +34,60 @@ const argv = yargs(hideBin(process.argv))
             console.log(`<Текущий день: ${now.getDate()}`)
         }
     })
+    .command('add', 'Добавить год/месяц/день', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'number',
+                description: 'Добавить год'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'number',
+                description: 'Добавить месяц'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'number',
+                description: 'Добавить день'
+            })
+    }, (argv) => {
+        if (argv.year) {
+            console.log(`<Текущий год: ${now.getFullYear() + argv.year}`)
+        }
+        if (argv.month) {
+            console.log(`<Текущий месяц: ${now.getMonth() + 1 + argv.month}`)
+        }
+        if (argv.day) {
+            console.log(`<Текущий день: ${now.getDate() + argv.day}`)
+        }
+    })
+    .command('sub', 'Отнять год/месяц/день', (yargs) => {
+        return yargs
+            .option('year', {
+                alias: 'y',
+                type: 'number',
+                description: 'Отнять год'
+            })
+            .option('month', {
+                alias: 'm',
+                type: 'number',
+                description: 'Отнять месяц'
+            })
+            .option('day', {
+                alias: 'd',
+                type: 'number',
+                description: 'Отнять день'
+            })
+    }, (argv) => {
+        if (argv.year) {
+            console.log(`<Текущий год: ${now.getFullYear() - argv.year}`)
+        }
+        if (argv.month) {
+            console.log(`<Текущий месяц: ${now.getMonth() + 1 - argv.month}`)
+        }
+        if (argv.day) {
+            console.log(`<Текущий день: ${now.getDate() - argv.day}`)
+        }
+    })
     .argv
-
-//TODO Добавить add и sub
-
